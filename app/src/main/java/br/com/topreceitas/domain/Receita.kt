40acecf268b676a0d1332581957a2e4bbaace0b9
UenoSilva <uenosouzasilva@gmil.com>
2024-04-24 18:17:13 -0300

    doc: adicionando imagens das receitas

--- app/src/main/java/br/com/topreceitas/domain/Receita.kt
+++ app/src/main/java/br/com/topreceitas/domain/Receita.kt
@@ -3,18 +3,40 @@ package br.com.topreceitas.domain
 import com.google.gson.annotations.SerializedName
 
 data class Receita(
-    @SerializedName("titulo") val title: String?,
-    @SerializedName("imagem") val image: String?,
-    @SerializedName("porcoes") val portion: Int,
-    @SerializedName("tempo") val timer: Int,
-    @SerializedName("categoria") val category: List<Categoria>?,
-    @SerializedName("ingredientes") val ingredients: List<String>?,
-    @SerializedName("preparo") val preparation: List<String>?,
-    @SerializedName("dicas") val tips: String?,
+    @SerializedName("titulo")
+    val title: String?,
+    @SerializedName("imagem")
+    val image: String?,
+    @SerializedName("porcoes")
+    val portion: Int,
+    @SerializedName("tempo")
+    val timer: Int,
+    @SerializedName("categoria")
+    val category: List<Categoria>?,
+    @SerializedName("ingredientes")
+    val ingredient: List<Ingredients>?,
+    @SerializedName("preparo")
+    val preparation: List<Preparo>?,
+    @SerializedName("dicas")
+    val tips: String?,
 
     var isFavorite: Boolean = false
 )
 
 data class Categoria(
     @SerializedName("tipo") val type: String?
+)
+
+data class Ingredients(
+    @SerializedName("ingredientes_titulo")
+    val ingredientes_titulo: String,
+    @SerializedName("ingredientes_ingredientes")
+    val ingredientes_ingredientes: List<String>
+)
+
+data class Preparo(
+    @SerializedName("preparo_titulo")
+    val preparo_titulo: String,
+    @SerializedName("preparo_modo")
+    val preparo_modo: List<String>
 )
